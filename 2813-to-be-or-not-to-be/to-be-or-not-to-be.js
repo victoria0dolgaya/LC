af5a45060b5763ea2d49d6426dae6b8bc3746941
Viktoriia Dolha <152037057+victoria0dolgaya@users.noreply.github.com>
2025-09-23 14:19:02 +0100

    Time: 49 ms (22.53%) | Memory: 54.3 MB (15.23%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,18 @@
+var expect = function(val) {
+    return {
+        toBe: function(otherVal) {
+            if (val === otherVal) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(otherVal) {
+            if (val !== otherVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    }
+};
